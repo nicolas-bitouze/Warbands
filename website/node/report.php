31b4a6d3ba6cdf871d7486726395a56fffd079a8
septIO <in@sept.io>
2015-08-03 01:46:19 +0200

    Added report.php to include the new json structure

---
+++ website/node/report.php
@@ -0,0 +1,11 @@
+<?php
+$timeslot = (int) (((time() / 60)) / 60);
+
+$dbh = new PDO('mysql:host=localhost;dbname=warbands', 'root', '');
+
+$warbands = $dbh -> prepare('SELECT name,band,dots,reports_on_710,last_report_text FROM maps WHERE timeslot=:timeslot AND (band!="none" AND dots>0) GROUP BY name');
+
+$warbands -> execute([':timeslot' => $ts]);
+$current = $warbands -> fetchAll(PDO::FETCH_ASSOC);
+$data = json_encode(['maps' => $current, 'timeslot' => $ts]);
+$file = file_put_contents('current_data.json', $data);
